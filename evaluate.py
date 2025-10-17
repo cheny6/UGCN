beb1792232c45469e83843a09045c84c4eff0908
cheny6 <284424621@qq.com>
2025-10-17 22:00:19 +0800

    Function porting for Torchlight version updates

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,5 @@
+import sys
+import traceback
 import yaml
 import random
 import argparse
@@ -7,11 +9,29 @@ from pathlib import Path
 import torch
 from torch import nn, optim
 
-from torchlight.io import DictAction
-from torchlight.io import import_class
-
 import numpy as np
 from tensorboardX import SummaryWriter
+
+class DictAction(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed")
+        super(DictAction, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        input_dict = eval(f'dict({values})')  #pylint: disable=W0123
+        output_dict = getattr(namespace, self.dest)
+        for k in input_dict:
+            output_dict[k] = input_dict[k]
+        setattr(namespace, self.dest, output_dict)
+        
+def import_class(import_str):
+    mod_str, _sep, class_str = import_str.rpartition('.')
+    __import__(mod_str)
+    try:
+        return getattr(sys.modules[mod_str], class_str)
+    except AttributeError:
+        raise ImportError('Class %s cannot be found (%s)' % (class_str, traceback.format_exception(*sys.exc_info())))
 
 parser = argparse.ArgumentParser(description='Evaluate resnet50 features on ImageNet')
 parser.add_argument('--dev', type=str, help='device')
